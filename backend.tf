67cc281796f9810e3092753f9e0b1b16eb1f73ed
Onyedikachi Ulelu <dirkaka@yahoo.com>
2024-01-25 08:43:41 +0000

    updated file

---
+++ backend.tf
@@ -0,0 +1,10 @@
+# store the terraform state file in s3 and lock with dynamodb
+terraform {
+  backend "s3" {
+    bucket         = "kach07-terraform-remote-state" # already create in the console
+    key            = "terraform-module/nestapp/terraform.tfstate"
+    region         = "eu-west-2"
+    profile        = "Mr.Kachy"
+    dynamodb_table = "terraform-state-lock"
+  }
+}
---
+++ main.tf
@@ -0,0 +1,134 @@
+locals {
+  region       = var.region
+  project_name = var.project_name
+  environment  = var.environment
+}
+
+# Create vpc module
+module "vpc" {
+  source                       = "git@github.com:Kach07/Terraform-modules.git//vpc"
+  region                       = local.region
+  project_name                 = local.project_name
+  environment                  = local.environment
+  vpc_cidr                     = var.vpc_cidr
+  public_subnet_az1_cidr       = var.public_subnet_az1_cidr
+  public_subnet_az2_cidr       = var.public_subnet_az2_cidr
+  private_app_subnet_az1_cidr  = var.private_app_subnet_az1_cidr
+  private_app_subnet_az2_cidr  = var.private_app_subnet_az2_cidr
+  private_data_subnet_az1_cidr = var.private_data_subnet_az1_cidr
+  private_data_subnet_az2_cidr = var.private_data_subnet_az2_cidr
+}
+
+# Create nat-gateways
+module "nat_gateway" {
+  source                     = "git@github.com:Kach07/Terraform-modules.git//nat-gateway"
+  project_name               = local.project_name              # referenced from local variable"
+  environment                = local.environment               # referenced from local variable"
+  public_subnet_az1_id       = module.vpc.public_subnet_az1_id # referenced from vpc module  as the public subnet az1 was created in the vpc module
+  internet_gateway           = module.vpc.internet_gateway     # referenced from vpc module  as the Internet gateway was created in the vpc module
+  public_subnet_az2_id       = module.vpc.public_subnet_az2_id # referenced from vpc module  as the public subnet az2 was created in the vpc module
+  vpc_id                     = module.vpc.vpc_id
+  private_app_subnet_az1_id  = module.vpc.private_app_subnet_az1_id
+  private_data_subnet_az1_id = module.vpc.private_data_subnet_az1_id
+  private_app_subnet_az2_id  = module.vpc.private_app_subnet_az2_id
+  private_data_subnet_az2_id = module.vpc.private_data_subnet_az2_id # referenced from vpc module  as the private data subnet az2 was created in the vpc module
+}
+
+#Create security-groups module
+module "security_group" {
+  source       = "git@github.com:Kach07/Terraform-modules.git//security-groups"
+  project_name = local.project_name # referenced from local variable"
+  environment  = local.environment  # referenced from local variable"
+  vpc_id       = module.vpc.vpc_id  # referenced from vpc module  as the vpc id
+  ssh_ip       = var.ssh_ip         # the value will be add to the terraform.tfvars file
+}
+
+# Launch rds instance
+module "rds" {
+  source                       = "git@github.com:Kach07/Terraform-modules.git//rds"
+  project_name                 = local.project_name
+  environment                  = local.environment
+  private_data_subnet_az1_id   = module.vpc.private_data_subnet_az1_id
+  private_data_subnet_az2_id   = module.vpc.private_data_subnet_az2_id
+  database_snapshot_identifier = var.database_snapshot_identifier # referenced from rds module 
+  database_instance_class      = var.database_instance_class
+  availability_zone_1          = module.vpc.availability_zone_1
+  database_instance_identifier = var.database_instance_identifier
+  multi_az_deployment          = var.multi_az_deployment
+  database_security_group_id   = module.security_group.database_security_group_id
+}
+
+# Request SSL Certificate
+module "ssl_certificate" {
+  source            = "git@github.com:Kach07/Terraform-modules.git//acm"
+  domain_name       = var.domain_name
+  alternative_names = var.alternative_names
+}
+
+# create alb module
+module "application_load_balancer" {
+  source                = "git@github.com:Kach07/Terraform-modules.git//alb"
+  project_name          = local.project_name
+  environment           = local.environment
+  alb_security_group_id = module.security_group.alb_security_group_id
+  public_subnet_az1_id  = module.vpc.public_subnet_az1_id
+  public_subnet_az2_id  = module.vpc.public_subnet_az2_id
+  target_type           = var.target_type
+  vpc_id                = module.vpc.vpc_id
+  certificate_arn       = module.ssl_certificate.certificate_arn
+}
+
+#create S3 bucket
+module "s3_bucket" {
+  source               = "git@github.com:Kach07/Terraform-modules.git//S3"
+  project_name         = local.project_name
+  env_file_bucket_name = var.env_file_bucket_name
+  env_file_name        = var.env_file_name
+}
+
+# create ecs task execution role
+module "ecs_task_execution_role" {
+  source               = "git@github.com:Kach07/Terraform-modules.git//iam-role"
+  project_name         = local.project_name
+  environment          = local.environment
+  env_file_bucket_name = module.s3_bucket.env_file_bucket_name
+}
+
+# create ecs cluster, task definition and service
+module "ecs" {
+  source                       = "git@github.com:Kach07/Terraform-modules.git//ecs"
+  project_name                 = local.project_name
+  environment                  = local.environment
+  ecs_task_execution_role_arn  = module.ecs_task_execution_role.ecs_task_execution_role_arn
+  architecture                 = var.architecture
+  container_image              = var.container_image
+  env_file_bucket_name         = module.s3_bucket.env_file_bucket_name
+  env_file_name                = module.s3_bucket.env_file_name
+  region                       = local.region
+  private_app_subnet_az1_id    = module.vpc.private_app_subnet_az1_id
+  private_app_subnet_az2_id    = module.vpc.private_app_subnet_az2_id
+  app_server_security_group_id = module.security_group.app_server_security_group_id
+  alb_target_group_arn         = module.application_load_balancer.alb_target_group_arn
+}
+
+# create auto scaling group
+module "asg_ecs" {
+  source       = "git@github.com:Kach07/Terraform-modules.git//asg-ecs"
+  project_name = local.project_name
+  environment  = local.environment
+  ecs_service  = module.ecs.ecs_service
+}
+
+# create record set in route-53
+module "route_53" {
+  source                             = "git@github.com:Kach07/Terraform-modules.git//route-53"
+  domain_name                        = module.ssl_certificate.domain_name
+  record_name                        = var.record_name
+  application_load_balancer_dns_name = module.application_load_balancer.application_load_balancer_dns_name
+  application_load_balancer_zone_id  = module.application_load_balancer.application_load_balancer_zone_id
+}
+
+# print the website url
+output "website_url" {
+  value = join("", ["https://", var.record_name, ".", var.domain_name])
+}
