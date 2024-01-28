407c37619c89a1a29add6fd239cbdd9732d83d09
Onyedikachi Ulelu <dirkaka@yahoo.com>
2024-01-28 23:43:23 +0000

    updated backend file

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 # store the terraform state file in s3 and lock with dynamodb
 terraform {
   backend "s3" {
-    bucket         = "kach07-terraform-remote-state" # already create in the console
+    bucket         = "kach07-terraform-state-file" # already create in the console
     key            = "terraform-module/nestapp/terraform.tfstate"
     region         = "eu-west-2"
     profile        = "Mr.Kachy"
